3a5ccc1e29f87009d2ddb15f13d61c8ff428f20e
Fabricio Bastian <fabriciobastian@gmail.com>
2021-01-30 22:15:16 +0100

    rename project to nice-ng-pipes

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -2,19 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LengthModule, NotModule, SkipModule } from 'ngx-async-pipeline';
+import { LengthModule, NotModule, SkipModule } from 'nice-ng-pipes';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    LengthModule,
-    NotModule,
-    SkipModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		LengthModule,
+		NotModule,
+		SkipModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
