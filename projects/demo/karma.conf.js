1f6ddbfcf8a620764e603c43f0e4011ec348e718
Fabricio Bastian <fabriciobastian@gmail.com>
2021-01-31 15:22:29 +0100

    config tests to run with ChromeHeadless and report on console demo project tests are still failing (no tests, probably a config issue)

--- projects/demo/karma.conf.js
+++ projects/demo/karma.conf.js
@@ -10,23 +10,34 @@ module.exports = function (config) {
 			require('karma-chrome-launcher'),
 			require('karma-jasmine-html-reporter'),
 			require('karma-coverage-istanbul-reporter'),
-			require('@angular-devkit/build-angular/plugins/karma')
+			require('@angular-devkit/build-angular/plugins/karma'),
+			require('karma-spec-reporter')
 		],
 		client: {
 			clearContext: false // leave Jasmine Spec Runner output visible in browser
 		},
 		coverageIstanbulReporter: {
 			dir: require('path').join(__dirname, '../../coverage/demo'),
-			reports: ['html', 'lcovonly', 'text-summary'],
+			reports: ['text-summary'],
 			fixWebpackSourcePaths: true
 		},
-		reporters: ['progress', 'kjhtml'],
+		reporters: ["spec"],
+		specReporter: {
+			maxLogLines: 5, // limit number of lines logged per test
+			suppressErrorSummary: false, // do not print error summary
+			suppressFailed: false, // do not print information about failed tests
+			suppressPassed: false, // do not print information about passed tests
+			suppressSkipped: true, // do not print information about skipped tests
+			showSpecTiming: true, // print the time elapsed for each spec
+			failFast: true, // test would finish with error when a first fail occurs.
+			failOnEmptyTestSuite: false
+		},
 		port: 9876,
 		colors: true,
 		logLevel: config.LOG_INFO,
 		autoWatch: true,
-		browsers: ['Chrome'],
-		singleRun: false,
+		browsers: ['ChromeHeadless'],
+		singleRun: true,
 		restartOnFileChange: true
 	});
 };
