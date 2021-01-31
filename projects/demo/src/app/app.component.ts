59100b5fa519c10d64f51a2d07045f61695334d7
Fabricio Bastian <fabriciobastian@gmail.com>
2021-01-31 13:55:36 +0100

    add .npmrc and update package-json, still some package issues

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -2,11 +2,9 @@ import { Component } from '@angular/core';
 import { of } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  template: `
-    {{ title$ | skip:3 | length | not | async }}
-  `,
+	selector: 'app-root',
+	templateUrl: 'app.component.html',
 })
 export class AppComponent {
-  title$ = of('demo');
+	title$ = of(['demo', 'demo1', 'demo2', 'demo3']);
 }
--- projects/nice-ng-pipes/src/lib/async/skip/skip.pipe.ts
+++ projects/nice-ng-pipes/src/lib/async/skip/skip.pipe.ts
@@ -4,11 +4,9 @@ import { skip } from 'rxjs/operators';
 
 @Pipe({ name: 'skip', pure: true })
 export class SkipPipe<T> implements PipeTransform {
-
-  transform(stream: Observable<T>, count: number = 0): Observable<T> {
-    return stream.pipe(
-      skip(count),
-    );
-  }
+	transform(stream: Observable<T>, count: number = 0): Observable<T> {
+		return stream.pipe(
+			skip(count),
+		);
+	}
 }
-
